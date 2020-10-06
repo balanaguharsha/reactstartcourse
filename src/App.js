ccbf722b90fda6e180fe32c4e97da93b99cf1005
Balanagu Harsha Vardhan <bavardha@microsoft.com>
2020-10-06 21:19:55 +0530

    Added Personalized message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <AppChild name={"Harsha"}/>
+      <AppChild name={"Vardhan"}/>
+    </div>
+  );
+}
+
+function AppChild(props) {
+  return (
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,11 +25,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from {props.name}
         </a>
       </header>
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
